c88e14c5ae8254b768434d2fb0561592a4125047
sawadash19 <shu19951019@icloud.com>
2022-10-13 19:03:15 +0900

    ファイル名の変更中

--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-//use App\Http\Controllers\MemberController;
-//use App\Models\Member;
+use App\Http\Controllers\MemberController;
+use App\Models\Member;
 Route::get('/', function () {
     return view('welcome');
 
 });
 
-Route::get('/top',[app\Http\Controllers\MemberController::class,'top'])->name('top');
+Route::get('/top',[MemberController::class,'top'])->name('top');
 
 Route::get('/register',[MemberController::class,'register'])->name('register');
 
